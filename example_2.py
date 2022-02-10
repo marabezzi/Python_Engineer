6693b7d186e5b46f2231c3ef20c45c4ccfeee08e
Guilherme A. Marabezzi Clerice <61362302+marabezzi@users.noreply.github.com>
2022-02-10 16:57:27 -0300

    3rd

--- example_2.py
+++ example_2.py
@@ -1,25 +1,15 @@
-import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 
-v = [2,4,6]
-w = [4,2,6]
+soa = np.array([[0, 0, 1, 1, -2, 0], [0, 0, 2, 1, 1, 0],
+                [0, 0, 3, 2, 1, 0], [0, 0, 4, 0.5, 0.7, 0]])
 
-array = np.array([[0, 0, v[0], v[1], v[2]], 
-                 [0, 0, w[0], w[1]], w[2]])
-
-X, Y, V, W = zip(*array)
-plt.figure()
-plt.ylabel('Y-axis')
-plt.xlabel('X-axis')
-ax = plt.gca()
-ax.annotate(f'v({v[0]},{v[1]},{v[2]})', (v[0],v[1],v[2]),fontsize=14)
-plt.scatter(v[0],v[1],v[3], s=10,c='red')
-ax.annotate(f'w({w[0]},{w[1]},{w[2]})', (w[0],w[1]),fontsize=14)
-plt.scatter(w[0], w[1], w[2], s=10,c='blue')
-ax.quiver(X, Y, V, W, angles='xy', scale_units='xy',color=['r','b'],scale=1)
-ax.set_xlim([0, 10])
-ax.set_ylim([0, 10])
-
-plt.grid()
-plt.draw()
+X, Y, Z, U, V, W = zip(*soa)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.quiver(X, Y, Z, U, V, W)
+ax.set_xlim([-1, 0.5])
+ax.set_ylim([-1, 1.5])
+ax.set_zlim([-1, 8])
 plt.show()
